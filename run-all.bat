ff5ad16d6af41a54fc1578c8de9db7f912513e7a
astems01 - kang <kangdh78@astems.co.kr>
2025-03-26 15:45:38 +0900

    2

--- run-all.bat
+++ run-all.bat
@@ -3,15 +3,15 @@ setlocal
 
 del /q logs\*.log >nul 2>&1
 
-javac GCPauseBenchmark.java
+javac GCBenchmark.java
 
 echo ▶ G1GC 테스트 중...
-java -cp . -Xms1g -Xmx1g -Xlog:gc*:file=logs\g1gc.log -XX:+UseG1GC GCBenchmarkTest
+java -cp . -Xms2g -Xmx2g -Xlog:gc*:file=logs\g1gc.log -XX:+UseG1GC GCBenchmark
 
 echo ▶ ZGC 테스트 중...
-java -cp . -Xms1g -Xmx1g -Xlog:gc*:file=logs\zgc.log -XX:+UseZGC GCBenchmarkTest
+java -cp . -Xms2g -Xmx2g -Xlog:gc*:file=logs\zgc.log -XX:+UseZGC GCBenchmark
 
 echo ▶ GenZGC 테스트 중...
-java -cp . -Xms1g -Xmx1g -Xlog:gc*:file=logs\genzgc.log -XX:+UseZGC -XX:+ZGenerational GCBenchmarkTest
+java -cp . -Xms2g -Xmx2g -Xlog:gc*:file=logs\genzgc.log -XX:+UseZGC -XX:+ZGenerational GCBenchmark
 
 echo 모든 테스트 완료. 로그는 logs 폴더 확인
